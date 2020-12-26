9c91e1feca09ad303e109a284f2beb1d2d070aa9
liskuba <56127478+liskuba@users.noreply.github.com>
2020-12-26 16:51:43 +0100

    Update main.py

--- data_preparation/main.py
+++ data_preparation/main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
 
     # Create your csv files
     # a list to all directories created by facebook
-    # sometimes it is only one directory, sometimes more - make you put it in a list
+    # sometimes it is only one directory, sometimes more - make sure to put it in a list
     # fe. list_of_paths_to_directories = ['facebook-kubalis186-1', 'facebook-kubalis186-2']
 
     list_of_paths_to_directories = None
