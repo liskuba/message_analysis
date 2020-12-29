6e73d8558b78c4e6fab479e24fa8ac693fa5fa3c
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2020-12-29 11:57:06 +0100

    updated paths to csv

--- R_plots/activity_time.R
+++ R_plots/activity_time.R
@@ -6,9 +6,9 @@ library(dplyr)
 # sciezka do katalogu z csv przygotowanymi w pythonie
 path <- "../data_preparation/data"
 
-messages <- read_csv(paste(path,"/Bartek_Sawicki_1.csv",sep = ''))
-messages <- rbind(messages, read_csv(paste(path,"/Kuba_Lis_1.csv",sep = '')))
-#messages <- rbind(messages, read_csv(paste(path,"/Kuba_Koziel_1.csv",sep = '')))
+messages <- read_csv(paste(path,"/messages_Bartek_Sawicki.csv",sep = ''))
+messages <- rbind(messages, read_csv(paste(path,"/messages_Kuba_Lis.csv",sep = '')))
+#messages <- rbind(messages, read_csv(paste(path,"/messages_Kuba_Koziel.csv",sep = '')))
 
 summary(messages)
 
