3a0bc94aa85bab30a126875de12636a06d867605
liskuba <01151424@pw.edu.pl>
2021-01-03 00:37:41 +0100

    cleanup

--- data_preparation/create_messages_csv.py
+++ data_preparation/create_messages_csv.py
@@ -73,22 +73,3 @@ def count_in_one_file(person, path_to_json, timestamps=None, lengths=None, emoji
                         e += key
                 emojis.append(e)
     return timestamps, lengths, emojis
-
-
-def count_in_all_files_2(person, list_of_paths_to_directories, print_paths=False):
-    """every directory should ends with '/'"""
-    pattern = "message_*.json"
-    timestamps = []
-    reactions = []
-    i = 0
-    for directory in list_of_paths_to_directories:
-        for path, subdirs, files in os.walk(directory + 'messages' + os.path.sep):
-            for name in files:
-                if fnmatch(name, pattern):
-                    if print_paths:
-                        i += 1
-                        print(f'{i}. {os.path.join(path, name)}')
-                    timestamps, reactions = count_in_one_file_2(
-                        person, os.path.join(path, name), timestamps, reactions
-                    )
-    return timestamps, reactions
